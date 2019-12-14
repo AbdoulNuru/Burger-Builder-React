d78ce039856205ad591b790c7ceedef0cd9c2cc3
Nuru <abdoulniyigena@gmail.com>
2019-12-14 22:32:35 +0200

    Using state and setState

--- src/App.js
+++ src/App.js
@@ -3,12 +3,44 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+state = {
+  persons: [
+    {name: 'Max', age: 28},
+    {name: 'Manu', age: 29},
+    {name: 'Stephanie', age: 26}
+  ]
+}
+
+switchNameHandler = () => {
+  this.setState({
+    persons: [
+      { name: "Maximilian", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 27 }
+    ],
+    otherstate: 'some other value'
+  });
+}
+
   render() {
     return (
       <div className="App">
         <h1>Hi am Abdoul</h1>
-        <Person name="Diouf" age="23" >My hobbies: Coding</Person>
-        <Person name="Bwiza" age="24" />
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My hobbies: Coding
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Woow am learning react'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,9 +5,9 @@ const person = (props) => {
         <div>
             <p>I'm {props.name} and I'm {props.age} 
             years old!!!</p>
-            <p>{props.children}</p>
+            <p> {props.children} </p>
         </div>
-    ) 
+    )
 }; 
 
 export default person;
