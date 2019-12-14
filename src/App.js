7a4f439670bcdfe9a0944c1166d3416795c34adc
Nuru <abdoulniyigena@gmail.com>
2019-12-14 21:13:19 +0200

    Making our component dynamic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi am Abdoul</h1>
+        <Person/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Woow am learning react'));
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = () => {
+    return <p>Creating a new Component for 
+         a {Math.floor(Math.random() * 30)}
+          th time!!</p> 
+}; 
+
+export default person;
