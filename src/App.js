0458fc14517d3c336a675224eed2bc06df00dbad
Nuru <abdoulniyigena@gmail.com>
2019-12-14 20:42:55 +0200

    Change the was of rendering jsx

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    //return (
-      //<div className="App">
-        //<h1>Hi am Abdoul</h1>
-      //</div>
-    //);
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Woow am learning react'));
+    return (
+      <div className="App">
+        <h1>Hi am Abdoul</h1>
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Woow am learning react'));
   }
 }
 
