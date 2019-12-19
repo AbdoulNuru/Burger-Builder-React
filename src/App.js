7e8e8c8fed8478ff751091a1352a745c4e8863b2
Nuru <abdoulniyigena@gmail.com>
2019-12-19 20:04:11 +0200

    add and use radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component{
@@ -48,7 +49,11 @@ togglePersonHandler = () =>{
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -68,6 +73,10 @@ togglePersonHandler = () =>{
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const styleClasses = [];
@@ -94,4 +103,4 @@ togglePersonHandler = () =>{
   }
 }
 
-export default App;
+export default Radium(App);
