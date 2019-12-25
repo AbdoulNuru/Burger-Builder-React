5df3ed31c2669b52ade7e360c8447e576800b8ef
Nuru <abdoulniyigena@gmail.com>
2019-12-25 14:10:13 +0200

    use useEffect hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log('[cockpit.js] useEffect');
+    // Http request...
+  });
+
     const styleClasses = [];
     let btnClass = '';
 
