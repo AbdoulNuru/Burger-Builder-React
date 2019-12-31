4714c2a52e48efd023e1e9c98742e812eeb0a22f
Nuru <abdoulniyigena@gmail.com>
2019-12-31 19:48:59 +0200

    use higher other components(hoc) with styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -85,7 +86,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={()=> {
           this.setState({showCockpit: false});
         }}
@@ -97,7 +98,7 @@ class App extends Component {
           clicked={this.togglePersonHandler}
         />) : null}
         {persons}
-      </div>
+      </WithClass>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Woow am learning react'));
   }
